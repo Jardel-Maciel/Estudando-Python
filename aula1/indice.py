bb414125665e9cbe908b8f1f1c2befdf217b9676
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-07 16:23:38 -0300

    Create indice.py

---
+++ aula1/indice.py
@@ -0,0 +1,15 @@
+frase = 'Ola, mundo!'
+print(frase)
+
+print(frase[0])
+print(frase[1])
+print(frase[2])
+print(frase[3])
+print(frase[4])
+print(frase[5])
+print(frase[6])
+print(frase[7])
+print(frase[8])
+print(frase[9])
+print(frase[10])
+
