37207a439f9781d52fe3382e4413db104fe14427
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-09 11:57:45 -0300

    manipulaação de dados

--- aula1/entrada_de_dados.py
+++ aula1/entrada_de_dados.py
@@ -1,2 +1,9 @@
+"""
 nome = input('Qual e o seu nome? ')
-print(nome)
+print(f'Ola {nome}, seja bem vindo!')
+idade = input('Qual e a sua idade? ')
+print(f'voce tem {idade} anos de idade')"""
+
+# convertendo dados de entrada (CASTING)
+nota = float(input('Qual nota voce recebeu na disciplina? ')) 
+print(f'voce tirou nota {nota}.')
