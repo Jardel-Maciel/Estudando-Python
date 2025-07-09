c35e35e14ceb813911250552122f033444ac2cd5
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-09 13:01:47 -0300

    calculo de desconto

---
+++ aula1/Aula_pratica/aula_pratia.py
@@ -0,0 +1,16 @@
+#print(403 % 73) 
+#print(2 ** 10)
+#print(abs(54 - 57))
+#print(min(34, 29, 31))
+"""
+a = 3
+b = 4
+c = a * a + b * b
+print(c)
+"""
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+print(s1 + ' ' + s2 + ' '+ s3)
+print((s1 + ' ' )* 10)
+print(s1 + ' ' + (s2 + ' ')*2 + ' ' + (s3 + ' ')*3)
---
+++ aula1/Aula_pratica/exercicio1.py
@@ -0,0 +1,5 @@
+preco = float(input('Qual e o valor do produto?'))
+perentual = float(input('Qual sera o desonto aplicado de (0-100%): '))
+
+desonto = preco *(perentual / 100)
+final =  preco - desonto 
