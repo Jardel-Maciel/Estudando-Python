cc494e3901771a0791c267efc95712cee755bbcb
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-10 23:34:27 -0300

    Create tipos_primitivos_e_saida.py

---
+++ curso_em_vídeo/aulas/tipos_primitivos_e_saida.py
@@ -0,0 +1,12 @@
+""""
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+s = n1 + n2
+print(f'A soma entre {n1} e {n2} vale, {s}')
+"""
+
+#n = int(input('Digite um valor: '))
+#print(type(n))
+
+n = (input('Digite um valor: '))
+print(n.isupper())
