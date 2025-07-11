670d13b057110fb811adc606c81c3a33fa01f238
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-11 16:08:13 -0300

    desafio

---
+++ curso_em_vídeo/desafios/desafio_008.py
@@ -0,0 +1,4 @@
+med = int(input('Digite um valor em metros: '))
+cent = med * 100
+mili = med *1000
+print(f'{med} metros, convertidos fica {cent} centimetros, e em milimetros fica {mili}.')
---
+++ curso_em_vídeo/desafios/desafio_009.py
@@ -0,0 +1,3 @@
+num = int(input('Digite um numero para nossa tabuada: '))
+tab = num * 1, num * 2
+print(f'{tab}')
