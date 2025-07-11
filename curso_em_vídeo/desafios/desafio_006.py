c272447801bc5ed2eef4b0ca84044133fa830119
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-11 01:16:34 -0300

    desafios

---
+++ curso_em_vídeo/desafios/desafio_006.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um numero: "))
+dob = num * 2
+tri = num * 3
+raiz = num / 2
+print(f'numero {num} , dobro {dob} , triplo {tri} , raiz {raiz}')
---
+++ curso_em_vídeo/desafios/desafio_007.py
@@ -0,0 +1,4 @@
+nota1 = float(input('Qual foi a sua primeira nota? '))
+nota2 = float(input('Qual foi a sua segunda nota? '))
+soma = nota1 + nota1 / 2
+print(f'A sua media é de {soma}')
