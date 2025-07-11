ce2f86ccf50e7a672732480a604a21a002f67d1f
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-11 17:49:54 -0300

    Update desafio_10.py

---
+++ curso_em_vídeo/desafios/desafio_10.py
@@ -0,0 +1,10 @@
+"""
+crie um programa que leia quanto uma pessoa tem na carteira
+e mostre quantos dolares ela pode comprar
+
+"""
+reais = float(input('Qunatos reais voce tem na carteira? '))
+dolar = 3.27
+
+print(f'Com R$ {reais}, voce consegue comprar ${reais / dolar:.2f}')
+
