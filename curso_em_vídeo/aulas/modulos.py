8c4823ae353ef1c2828c22bf037c7d857d573381
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-13 01:51:12 -0300

    Create modulos.py

---
+++ curso_em_vídeo/aulas/modulos.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+"""
+import random
+num = random.randint(1,10)
+print(num)
+"""
+
+
+
+"""
+from math import sqrt, floor
+num = int(input('Digite em número: '))
+raiz = math.sqrt(num)
+print(f'A raiz de {num}, é igual a {raiz }')
+"""
