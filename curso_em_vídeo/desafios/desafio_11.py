5951bdff3ae6383f8fb2b540346ea5e3b2148cdc
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-12 22:52:09 -0300

    Update desafio_11.py

--- curso_em_vídeo/desafios/desafio_11.py
+++ curso_em_vídeo/desafios/desafio_11.py
@@ -1,5 +1,10 @@
 altura = float(input('Qual e a altura da parede? '))
 largura = float(input('Qual e a largura da parede? '))
-area = (altura + largura) * 2
+area = altura * largura
+
+litros_de_tinta = area / 2
+
 print(area)
+print(litros_de_tinta)
+
 
