d554d0d8c397c01c19fd0262e130a0505d742e49
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-14 23:31:39 -0300

    aulas

--- curso_em_vídeo/aulas/manipuando_textos.py
+++
@@ -1,8 +0,0 @@
-frase = 'curso em vídeo python'
-print(frase[9:13])
-print(frase[9:14])
-print(frase[9:21])
-print(frase[9:21:2])
-print(frase[:5])
-print(frase[15:])
-print(frase[9::3])
---
+++ curso_em_vídeo/aulas/manipulando_textos.py
@@ -0,0 +1,26 @@
+frase = 'curso em video python'
+#fatiamento
+'''print(frase[9:13])
+print(frase[9:14])
+print(frase[9:21])
+print(frase[9:21:2])
+print(frase[:5])
+print(frase[15:])
+print(frase[9::3])'''
+#análise
+'''
+print(len(frase))
+print(frase.count('o', 0, 13))
+print(frase.find('deo'))
+print('curso'in frase)
+'''
+#transformação
+'''print(frase.replace('python','android'))
+print(frase.upper())
+print(frase.lower())
+print(frase.capitalize())
+print(frase.title())
+print(frase.strip())'''
+#divisão
+frase.split()
+#junção
