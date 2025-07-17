b46c961c10c3004c8c5986de9ba4cd64af0576a4
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-17 18:29:35 -0300

    Create desafio_42.py

---
+++ curso_em_vídeo/desafios/mundo_2/desafio_42.py
@@ -0,0 +1,7 @@
+r1 = float(input('Digite o valor de uma resta: '))
+r2 = float(input('Digite outro valor: '))
+r3 =  float(input('Digite oultimo valor: '))
+if r1 + r2 > r3:
+    print('Um triangulo pode ser formado!')
+else:
+    print('O triangulo não pode ser formado: ')
