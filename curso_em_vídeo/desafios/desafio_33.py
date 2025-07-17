f14537d6d57077fd9b0fa7645344c199a72065ee
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-16 23:05:49 -0300

    Update desafio_33.py

--- curso_em_vídeo/desafios/desafio_33.py
+++ curso_em_vídeo/desafios/desafio_33.py
@@ -1,10 +1,9 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 n3 = int(input('Digite mais um número: '))
-
-if n1 > n2 > n3:
-    print(f'O numero maior é o {n1}')
-if n2 > n1 > n3:
-    print(f'O numero maior é {n2}')
-else:
-    print(f'O número maior é o {n3}')
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+print(f'O menor numero é o {menor}')
