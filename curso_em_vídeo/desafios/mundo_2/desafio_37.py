70b895b08e2e264586dee2772ff69a56807426e3
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-23 17:57:40 -0300

    Update desafio_37.py

--- curso_em_vídeo/desafios/mundo_2/desafio_37.py
+++ curso_em_vídeo/desafios/mundo_2/desafio_37.py
@@ -5,8 +5,10 @@ print('''Escolha uma das bases para conversão:
 [ 3 ] converter para HEXADECIMAL''')
 opcao = int(input('Sua opção: '))
 if opcao == 1:
-    print(f'{num} convertido para BINÁRIO é igual a {bin(num)} ')
+    print(f'{num} convertido para BINÁRIO é igual a {bin(num)[2:]} ')
 elif opcao == 2:
-    print(f'{num} convertido para OCTAL é igual a {oct(num)}')
+    print(f'{num} convertido para OCTAL é igual a {oct(num)[2:]}')
 elif opcao == 3:
-    print(f'{num} convertido para HEXADECIMAL é igual a {hex(num)}')
+    print(f'{num} convertido para HEXADECIMAL é igual a {hex(num)[2:]}')
+else:
+    print('Opção inválida tente novamente...')
