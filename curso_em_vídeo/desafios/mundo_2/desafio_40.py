15d45302689ae1a2b6519f2bacfb33ae06b249d0
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-23 18:26:44 -0300

    Update desafio_40.py

--- curso_em_vídeo/desafios/mundo_2/desafio_40.py
+++ curso_em_vídeo/desafios/mundo_2/desafio_40.py
@@ -1,12 +1,10 @@
 nota1 = float(input('Digite a nota do aluno: '))
 nota2 = float(input('Digite a segunda nota do aluno: '))
 media = (nota1 + nota2) / 2
-if media < 5.0:
-    print(f'A média do aluno é {media}')
+print(f'A média do aluno é {media}')
+if media < 5.0:   
     print('REPROVADO')
-elif media >= 5.0 and media < 7:
-    print(f'A média do aluno é {media}')
+elif media >= 5.0 and media < 7:    
     print('RECUPERAÇÃO')
-else:
-    print(f'A média do aluno é {media}')
+else:    
     print('APROVADO')
