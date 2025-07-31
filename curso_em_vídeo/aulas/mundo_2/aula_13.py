01c891799dbfb10550883d58047d8df832806d61
Jardel Maciel <jardel.maciel22@gmail.com>
2025-07-31 00:48:04 -0300

    Create aula_13.py

---
+++ curso_em_vídeo/aulas/mundo_2/aula_13.py
@@ -0,0 +1,9 @@
+#for c in range(0, 10, 2):
+#    print(c)
+#print('fim')
+i = int(input('Digite um número: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('Fim')
