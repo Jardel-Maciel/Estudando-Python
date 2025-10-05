8aee15d83543c441e7bdc6fa021fdaee6484261d
Jardel Maciel <jardel.maciel22@gmail.com>
2025-10-05 08:16:05 -0300

    Update desafio_50.py

--- curso_em_vídeo/desafios/mundo_2/desafio_50.py
+++ curso_em_vídeo/desafios/mundo_2/desafio_50.py
@@ -1,5 +1,6 @@
-for num in range(1, 7):
+soma = 0 
+for c in range(1, 7):
     num = int(input('Escolha um numero: '))
     if num % 2 == 0:
-        soma = num + num
+        soma += num
 print(f'A soma dos numeros pares é igual a {soma}')    
