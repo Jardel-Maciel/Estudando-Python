76a67d9cb8813e06fdaeb041093a435a1392c214
Jardel Maciel <jardel.maciel22@gmail.com>
2025-10-05 08:01:24 -0300

    Update desafio_48.py

--- curso_em_vídeo/desafios/mundo_2/desafio_48.py
+++ curso_em_vídeo/desafios/mundo_2/desafio_48.py
@@ -1,5 +1,7 @@
 soma = 0
+cont = 0
 for numero in range(1, 501, 2):  # apenas ímpares
     if numero % 3 == 0:
+        cont = cont + 1
         soma += numero
-print(f'A soma de todos o multiplos por 3 e igual a {soma}') 
+print(f'A soma de todos os {cont} valores é igual a {soma}') 
